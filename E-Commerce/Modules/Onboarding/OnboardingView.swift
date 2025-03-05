ea99e3e4ddf4c318f2fb63ad7e82374ca450ccc1
AnnaMelekh <annasmelekhina@gmail.com>
2025-03-05 12:13:24 +0100

    created rounded View, OnboardingVC

--- E-Commerce/Modules/Onboarding/OnboardingView.swift
+++ E-Commerce/Modules/Onboarding/OnboardingView.swift
@@ -7,6 +7,88 @@
 
 import UIKit
 
-class OnboardingView: UIView {
+final class OnboardingView: UIView {
+        
+    private lazy var imageView: UIImageView = {
+            let imageView = UIImageView()
+            imageView.contentMode = .scaleAspectFill
+            imageView.translatesAutoresizingMaskIntoConstraints = false
+            return imageView
+        }()
+        
+        private lazy var titleLabel: UILabel = {
+            let label = UILabel()
+            label.font = .custom(font: CustomFont.ralewayBlack, size: 20)
+            label.textColor = .black
+            label.numberOfLines = 1
+            label.adjustsFontSizeToFitWidth = true
+            label.minimumScaleFactor = 0.8
+            label.translatesAutoresizingMaskIntoConstraints = false
+            return label
+        }()
+        
+        private lazy var descriptionLabel: UILabel = {
+            let label = UILabel()
+            label.font = .custom(font: CustomFont.nunito, size: 15)
+            label.textColor = .darkGray
+            label.numberOfLines = 3
+            label.adjustsFontSizeToFitWidth = true
+            label.minimumScaleFactor = 0.8
+            label.translatesAutoresizingMaskIntoConstraints = false
+            return label
+        }()
     
+    private lazy var startButton = UIButton()
+        
+    init(image: UIImage?, title: String, description: String, button: UIButton? = nil) {
+            super.init(frame: .zero)
+            
+            setupView()
+            configure(image: image, title: title, description: description)
+        }
+        
+        required init?(coder: NSCoder) {
+            fatalError("init(coder:) has not been implemented")
+        }
+        
+        func configure(image: UIImage?, title: String, description: String) {
+            imageView.image = image
+            titleLabel.text = title
+            descriptionLabel.text = description
+        }
+    }
+
+private extension OnboardingView {
+    func setupView() {
+        backgroundColor = .white
+        layer.cornerRadius = 20
+        layer.shadowColor = UIColor(named: "DeliveryAddressTextColor")?.cgColor
+        layer.shadowOpacity = 0.2
+        layer.shadowOffset = CGSize(width: 0, height: 5)
+        layer.shadowRadius = 10
+        translatesAutoresizingMaskIntoConstraints = false
+        startButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        let stackView = UIStackView(arrangedSubviews: [titleLabel, descriptionLabel])
+        stackView.axis = .vertical
+        stackView.alignment = .center
+        stackView.spacing = 8
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        addSubview(imageView)
+        addSubview(stackView)
+        
+        NSLayoutConstraint.activate([
+            imageView.topAnchor.constraint(equalTo: topAnchor),
+            imageView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            imageView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            imageView.heightAnchor.constraint(equalToConstant: 200),
+            
+            stackView.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 10),
+            stackView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16),
+            stackView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -16)
+
+        ])
+    }
 }
+
--- E-Commerce/Modules/Onboarding/OnboardingViewController.swift
+++ E-Commerce/Modules/Onboarding/OnboardingViewController.swift
@@ -8,11 +8,112 @@
 import UIKit
 
 final class OnboardingViewController: UIViewController {
-
+    
+    //MARK: UIElements
+    
+    private lazy var imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "BubblesBG")
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    private lazy var pageControl: UIPageControl = {
+        let pageControl = UIPageControl()
+        pageControl.numberOfPages = 4
+        pageControl.currentPage = currentPage
+        pageControl.pageIndicatorTintColor = UIColor(named: "ItemsBackgroundColor")
+        pageControl.currentPageIndicatorTintColor = UIColor(named: "ButtonColor")
+        pageControl.isUserInteractionEnabled = false
+        pageControl.translatesAutoresizingMaskIntoConstraints = false
+        
+        
+        return pageControl
+    }()
+    
+    private lazy var startButton: UIButton = {
+        let startButton = UIButton()
+        startButton.setTitle("Start", for: .normal)
+        startButton.tintColor = UIColor(named: "ButtonColor")
+        startButton.isHidden = true
+        startButton.translatesAutoresizingMaskIntoConstraints = false
+        startButton.addTarget(self, action: #selector(startPressed), for: .touchUpInside)
+        return startButton
+    }()
+    
+    private lazy var view1 = OnboardingView(image: UIImage(named: "product01"), title: "Welcome!", description: "Discover a fast and easy way to shop online.")
+    private lazy var view2 = OnboardingView(image: UIImage(named: "product02"), title: "Smart Search & Favorites", description: "Find products instantly and save favorites for later.")
+    private lazy var view3 = OnboardingView(image: UIImage(named: "men"), title: "Easy Checkout", description: "Add to cart, choose payment, and order in seconds.")
+    private lazy var view4 = OnboardingView(image: UIImage(named: "electr"), title: "Manage Your Store", description: "Become a manager, add products, and control your catalog!", button: startButton)
+    
+    private var currentPage = 0
+    
+    //MARK: Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        setupUI()
+    }
+}
 
-        // Do any additional setup after loading the view.
+//MARK: private methods
+
+private extension OnboardingViewController {
+    func setupUI() {
+        view.backgroundColor = .white
+        view.addSubview(imageView)
+        view.addSubview(startButton)
+        view.addSubview(pageControl)
+        view.addSubview(view1)
+        view.addSubview(view2)
+        view.addSubview(view3)
+        view.addSubview(view4)
+        
+        setupConstraints()
     }
     
+    func setupConstraints() {
+        NSLayoutConstraint.activate([
+                imageView.topAnchor.constraint(equalTo: view.topAnchor),
+                imageView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+                imageView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+                imageView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+            ])
+            
+            let views = [view1, view2, view3, view4]
+            views.forEach { view in
+                view.translatesAutoresizingMaskIntoConstraints = false
+                NSLayoutConstraint.activate([
+                    view.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
+                    view.centerYAnchor.constraint(equalTo: self.view.centerYAnchor),
+                    view.widthAnchor.constraint(equalToConstant: 300),
+                    view.heightAnchor.constraint(equalToConstant: 500)
+                ])
+                view.isHidden = true
+            }
+            
+            view1.isHidden = false
+            
+            NSLayoutConstraint.activate([
+                startButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -50),
+                startButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 32),
+                startButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -32),
+                startButton.heightAnchor.constraint(equalToConstant: 50),
+    
+                pageControl.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -10),
+                pageControl.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+              
+            ])
+    }
+    
+    @objc func startPressed() {
+        let vc = TabBarViewController()
+        vc.modalPresentationStyle = .fullScreen
+        present(vc, animated: true)
+    }
 }
+
+#Preview { OnboardingViewController() }
